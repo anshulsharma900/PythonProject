ff8e02dd7b8afa1bb3215845181b995934a1bffd
anshulsharma900 <151901310+anshulsharma900@users.noreply.github.com>
2025-04-22 14:50:56 +0530

    Update PProject.py

--- PProject.py
+++ PProject.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the CSV file
-df = pd.read_csv("Provisional_COVID-19_Deaths_by_Sex_and_Age .csv")
+123df = pd.read_csv("Provisional_COVID-19_Deaths_by_Sex_and_Age .csv")
 print(df.head())
 print(df.info())
 
